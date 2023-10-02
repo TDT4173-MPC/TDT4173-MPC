e6a1fe9ea25db38391fbb1704b003d1448a83fa2
Mathias Otnes <Mathias.otnes@gmail.com>
2023-10-02 14:28:23 +0200

    Added logging and error handling to preprocessing script

--- Mathias/pipeline/preprocessing.sh
+++ Mathias/pipeline/preprocessing.sh
@@ -35,14 +35,22 @@ fi
 #                           Preprocessing pipeline                             #
 ################################################################################
 
-echo "Starting preprocessing..."
+echo "Starting preprocessing..." | tee -a $LOG_FILE
 
 SCRIPT_PATH="./Mathias/pipeline"
 DATA_PATH="./Mathias/pipeline/data"
+LOG_FILE="./Mathias/pipeline/preprocessing.log"
+
+# Clear the log file
+> $LOG_FILE
 
 # Import data and resample to 1 hour intervals
 FILES=$(python3 $SCRIPT_PATH/import.py)
-echo -e "\nFiles to process:\n$FILES\n"
+if [ $? -ne 0 ]; then
+    echo "Error during data import. Exiting." | tee -a $LOG_FILE
+    exit 1
+fi
+echo -e "\nFiles to process:\n$FILES\n" | tee -a $LOG_FILE
 
 # Remove columns that are not needed
 COLUMNS_TO_KEEP="\
@@ -55,25 +63,38 @@ pv_measurement \
 diffuse_rad_1h:J \
 pressure_100m:hPa"
 
-echo -e "Keeping columns:\n$COLUMNS_TO_KEEP\n"
+echo -e "Keeping columns:\n$COLUMNS_TO_KEEP\n" | tee -a $LOG_FILE
 
 for file in $FILES; do
-    python3 $SCRIPT_PATH/keep_columns.py "$COLUMNS_TO_KEEP" $DATA_PATH/$file
+    python3 $SCRIPT_PATH/keep_columns.py "$COLUMNS_TO_KEEP" $DATA_PATH/$file 2>&1 | tee -a $LOG_FILE
+    if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        echo "Error processing columns for $file. Exiting." | tee -a $LOG_FILE
+        exit 1
+    fi
 done
 
-
 # Removing outliers and NaN rows
-echo -e "\nRemoving outliers and NaN rows..."
+echo -e "\nRemoving outliers and NaN rows..." | tee -a $LOG_FILE
 for file in $FILES; do
-    python3 $SCRIPT_PATH/remove_outliers.py $DATA_PATH/$file
+    python3 $SCRIPT_PATH/remove_outliers.py $DATA_PATH/$file 2>&1 | tee -a $LOG_FILE
+    if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        echo "Error removing outliers for $file. Exiting." | tee -a $LOG_FILE
+        exit 1
+    fi
 done
 
 # Normalize data
-echo -e "\nNormalizing data..."
+echo -e "\nNormalizing data..." | tee -a $LOG_FILE
 for file in $FILES; do
-    python3 $SCRIPT_PATH/normalize.py $DATA_PATH/$file
+    python3 $SCRIPT_PATH/normalize.py $DATA_PATH/$file 2>&1 | tee -a $LOG_FILE
+    if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        echo "Error normalizing $file. Exiting." | tee -a $LOG_FILE
+        exit 1
+    fi
 done
 
-echo -e "\nPreprocessing completed\n"
+echo -e "\nPreprocessing completed\n" | tee -a $LOG_FILE
+
+
 
 
